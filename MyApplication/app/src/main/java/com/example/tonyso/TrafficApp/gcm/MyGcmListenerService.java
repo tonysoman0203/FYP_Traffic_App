c63f60003034edbee9c8fbcc656528bede9889f6
somankit2000 <somankit2000@gmail.com>
2016-03-25 22:25:06 +0800

    GCM Initialize Implementation

--- MyApplication/app/src/main/java/com/example/tonyso/TrafficApp/gcm/MyGcmListenerService.java
+++ MyApplication/app/src/main/java/com/example/tonyso/TrafficApp/gcm/MyGcmListenerService.java
@@ -16,6 +16,7 @@
 
 package com.example.tonyso.TrafficApp.gcm;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -23,7 +24,6 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.example.tonyso.TrafficApp.MainActivity;
@@ -93,23 +93,28 @@ public class MyGcmListenerService extends GcmListenerService {
     // [END receive_message]
 
     private void sendNotification(String title, String message) {
+        if (message.contains("<br>")) {
+            message = message.replace("<br>", "\n");
+        }
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+        Notification notificationBuilder = new Notification.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
+                .setStyle(new Notification.BigTextStyle().bigText(message))
                 .setContentTitle(title)
                 .setContentText(message)
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setContentIntent(pendingIntent)
+                .build();
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder);
     }
 }
